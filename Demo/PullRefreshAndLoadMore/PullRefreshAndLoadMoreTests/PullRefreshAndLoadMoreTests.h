17eeb0f8da3cdc625365c7c3e1f7d1b45214294b
akinLiu <hengxin196@gmail.com>
2012-04-18 13:46:32 +0800

    Add Demo project PullRefreshAndLoadMore

---
+++ Demo/PullRefreshAndLoadMore/PullRefreshAndLoadMoreTests/PullRefreshAndLoadMoreTests.h
@@ -0,0 +1,13 @@
+//
+//  PullRefreshAndLoadMoreTests.h
+//  PullRefreshAndLoadMoreTests
+//
+//  Created by akin liu on 12-4-18.
+//  Copyright (c) 2012年 hengxinsoft.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PullRefreshAndLoadMoreTests : SenTestCase
+
+@end
---
+++ Demo/PullRefreshAndLoadMore/PullRefreshAndLoadMoreTests/PullRefreshAndLoadMoreTests.m
@@ -0,0 +1,32 @@
+//
+//  PullRefreshAndLoadMoreTests.m
+//  PullRefreshAndLoadMoreTests
+//
+//  Created by akin liu on 12-4-18.
+//  Copyright (c) 2012年 hengxinsoft.com. All rights reserved.
+//
+
+#import "PullRefreshAndLoadMoreTests.h"
+
+@implementation PullRefreshAndLoadMoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PullRefreshAndLoadMoreTests");
+}
+
+@end
